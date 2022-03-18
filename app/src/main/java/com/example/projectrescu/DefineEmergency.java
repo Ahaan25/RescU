2b4a2a64e16c0a5e5e3d4d03b82d77b3ed2736b7
Ahaan25 <ahaan25092002@gmail.com>
2022-03-18 18:52:12 +0530

    Basic GUI

--- app/src/main/java/com/example/projectrescu/DefineEmergency.java
+++ app/src/main/java/com/example/projectrescu/DefineEmergency.java
@@ -2,15 +2,32 @@ package com.example.projectrescu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class DefineEmergency extends AppCompatActivity {
-    private Button button;
+    Button button;
+    EditText inp;
+    String st;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_define_emergency);
-        button=(Button) findViewById(R.id.button);
+        button=(Button) findViewById(R.id.button2);
+        inp=(EditText) findViewById(R.id.editTextTextPersonName);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(DefineEmergency.this, HomeActivity.class);
+                st=inp.getText().toString();
+                intent.putExtra("Emergency: ", st);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/projectrescu/HomeActivity.java
+++ app/src/main/java/com/example/projectrescu/HomeActivity.java
@@ -6,15 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class HomeActivity extends AppCompatActivity {
-    private Button button;
-
+    TextView outp;
+    String st;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        outp=findViewById(R.id.textView7);
+        st=getIntent().getExtras().getString("Emergency: ");
+        outp.setText(st);
 
         button=(Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -28,5 +35,6 @@ public class HomeActivity extends AppCompatActivity {
     public void openDefineEmergency(){
         Intent intent=new Intent(this, DefineEmergency.class);
         startActivity(intent);
+
     }
 }
