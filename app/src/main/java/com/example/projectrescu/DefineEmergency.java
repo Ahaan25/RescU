0a53f921c3ed2c87f92913936b58a161d0eb42a4
Ahaan25 <ahaan25092002@gmail.com>
2022-03-19 10:47:54 +0530

    Basic GUI

--- app/src/main/java/com/example/projectrescu/DefineEmergency.java
+++ app/src/main/java/com/example/projectrescu/DefineEmergency.java
@@ -1,17 +1,23 @@
 package com.example.projectrescu;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class DefineEmergency extends AppCompatActivity {
     Button button2;
     EditText inp;
     String st;
+    Switch s;
+    TextView loc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,5 +35,34 @@ public class DefineEmergency extends AppCompatActivity {
                 finish();
             }
         });
+// switch button location prompt code here
+        s=(Switch) findViewById(R.id.switch1);
+        loc=(TextView) findViewById(R.id.textView10);
+        s.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(DefineEmergency.this);
+                builder.setCancelable(true);
+                builder.setTitle("This emergency will send location details to emergency contacts");
+                builder.setMessage("This is an alert");
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+
+                builder.setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        loc.setVisibility(View.VISIBLE);
+                    }
+                });
+                builder.show();
+            }
+        });
+
+//prompt code ends here
     }
 }
