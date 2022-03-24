d573d08d43ed4ceba1aa7946ef0f90129c6538cc
Ahaan25 <ahaan25092002@gmail.com>
2022-03-24 13:19:11 +0530

    Basic GUI

--- app/src/main/java/com/example/projectrescu/DefineEmergency.java
+++ app/src/main/java/com/example/projectrescu/DefineEmergency.java
@@ -15,15 +15,20 @@ import android.widget.TextView;
 public class DefineEmergency extends AppCompatActivity {
     Button button2;
     EditText inp;
+    EditText nameno;
+    EditText message;
     String st;
     Switch s;
     TextView loc;
+    Button button3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_define_emergency);
         button2=(Button) findViewById(R.id.button2);
         inp=(EditText) findViewById(R.id.editTextTextPersonName);
+        nameno=(EditText) findViewById(R.id.editTextTextPersonName2);
+        message=(EditText) findViewById(R.id.editTextTextPersonName3);
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,5 +70,21 @@ public class DefineEmergency extends AppCompatActivity {
         });
 
 //prompt code ends here
+
+//discard button code here
+
+            button3=(Button) findViewById(R.id.button3);
+            button3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    s.setChecked(false);
+                    inp.setText(null);
+                    nameno.setText(null);
+                    message.setText(null);
+                }
+            });
+
+//discard code ends here
+
     }
 }
